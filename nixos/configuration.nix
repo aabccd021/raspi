6bf341dea187c06366dc24e2b699a2c5594b0b92
aabccd021 <aabccd021@gmail.com>
2022-05-08 18:40:53 +0700

    Initial commit

---
+++ nixos/configuration.nix
@@ -0,0 +1,50 @@
+{ config, pkgs, lib, ... }:
+{
+  # NixOS wants to enable GRUB by default
+  boot.loader.grub.enable = false;
+
+  # if you have a Raspberry Pi 2 or 3, pick this:
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+
+  # A bunch of boot parameters needed for optimal runtime on RPi 3b+
+  boot.kernelParams = ["cma=256M"];
+  boot.loader.raspberryPi.enable = true;
+  boot.loader.raspberryPi.version = 3;
+  boot.loader.raspberryPi.uboot.enable = true;
+  boot.loader.raspberryPi.firmwareConfig = ''
+    gpu_mem=256
+  '';
+  environment.systemPackages = with pkgs; [
+    libraspberrypi
+    vim
+  ];
+
+  # File systems configuration for using the installer's partition layout
+  fileSystems = {
+    "/" = {
+      device = "/dev/disk/by-label/NIXOS_SD";
+      fsType = "ext4";
+    };
+  };
+
+  # Preserve space by sacrificing documentation and history
+  documentation.nixos.enable = false;
+  nix.gc.automatic = true;
+  nix.gc.options = "--delete-older-than 30d";
+  boot.cleanTmpDir = true;
+
+  # Configure basic SSH access
+  services.openssh.enable = true;
+  services.openssh.permitRootLogin = "yes";
+
+  networking = {
+    defaultGateway = "192.168.1.1";
+    hostName = "raspi3b";
+    interfaces.eth0 = {
+      ipv4.addresses = [{
+        address = "192.168.1.2";
+        prefixLength = 24;
+      }];
+    };
+  };
+}
